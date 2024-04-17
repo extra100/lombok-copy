72faae39554ce5a59c1b3443178ea7179b1f743a
extra100 <bohhari224@gmail.com>
2024-04-17 09:33:22 +0700

    clearence

--- frontend/src/pages/OAuthLogin.tsx
+++ frontend/src/pages/OAuthLogin.tsx
@@ -1,70 +1,70 @@
-// // frontend/src/OAuthLogin.tsx
-// import React, { useEffect, useState } from 'react'
-// import { CLIENT_ID, CLIENT_SECRET, HOST, REDIRECT_URI } from '../config'
+// frontend/src/OAuthLogin.tsx
+import React, { useEffect, useState } from 'react'
+import { CLIENT_ID, CLIENT_SECRET, HOST, REDIRECT_URI } from '../config'
 
-// interface Tag {
-//   id: number
-//   name: string
-//   color: string | null
-//   purchase_trans_count: number
-//   sale_trans_count: number
-//   expense_trans_count: number
-//   fixed_asset_trans_count: number
-//   other_trans_count: number
-// }
+interface Tag {
+  id: number
+  name: string
+  color: string | null
+  purchase_trans_count: number
+  sale_trans_count: number
+  expense_trans_count: number
+  fixed_asset_trans_count: number
+  other_trans_count: number
+}
 
-// interface ApiResponse {
-//   success: boolean
-//   data: {
-//     data: Tag[]
-//   }
-// }
+interface ApiResponse {
+  success: boolean
+  data: {
+    data: Tag[]
+  }
+}
 
-// const OAuthLogin: React.FC = () => {
-//   const [tags, setTags] = useState<Tag[]>([])
+const OAuthLogin: React.FC = () => {
+  const [tags, setTags] = useState<Tag[]>([])
 
-//   useEffect(() => {
-//     const handleAuthentication = async () => {
-//       try {
-//         const response = await fetch(`${HOST}/banks`)
+  useEffect(() => {
+    const handleAuthentication = async () => {
+      try {
+        const response = await fetch(`${HOST}/banks`)
 
-//         const data: ApiResponse = await response.json()
-//         console.log(data)
+        const data: ApiResponse = await response.json()
+        console.log(data)
 
-//         if (data && data.success && data.data && data.data.data) {
-//           setTags(data.data.data)
-//         } else {
-//           console.error('Invalid API response:', data)
-//         }
-//       } catch (error) {
-//         console.error('Error during authentication:', error)
-//       }
-//     }
+        if (data && data.success && data.data && data.data.data) {
+          setTags(data.data.data)
+        } else {
+          console.error('Invalid API response:', data)
+        }
+      } catch (error) {
+        console.error('Error during authentication:', error)
+      }
+    }
 
-//     handleAuthentication()
-//   }, [])
+    handleAuthentication()
+  }, [])
 
-//   return (
-//     <div>
-//       <h1>Tags</h1>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <th>Nama Tag</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {tags.map((tag) => (
-//             <tr key={tag.id}>
-//               <td>{tag.id}</td>
-//               <td>{tag.name}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <h1>Tags</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nama Tag</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tags.map((tag) => (
+            <tr key={tag.id}>
+              <td>{tag.id}</td>
+              <td>{tag.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-// export default OAuthLogin
+export default OAuthLogin
---
+++ frontend/src/pages/financeInvoices/tesIngatan.md
@@ -0,0 +1,14 @@
+// const filteredData = invoiceData.filter((item: Invoice) => {
+// const startDate =
+// dateRange && dateRange[0] ? formatedDate(dateRange[1]) : null
+// const endDate = dateRange && dateRange[1] ? formatedDate(dateRange[1]) : null
+
+// const isDateMatching =
+// (!startDate || moment(item.trans_date).isSameOrBefore(startDate, 'day')) &&
+// (!endDate || moment(item.trans_date).isSameOrAfter(endDate, 'day'))
+
+// const isWhMatching =
+// selectedWh.length === 0 || selectedWh.includes(item.warehouse.name)
+
+// return isDateMatching && isDateMatch
+// })
